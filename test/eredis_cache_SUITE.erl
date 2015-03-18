a9391fc0a3fec605733c7166c9599c1638659849
Riccardo Massari <riccardo.massari@ubnt.com>
2015-03-18 21:40:02 +0100

    Refactor and improve tests

--- test/eredis_cache_SUITE.erl
+++ test/eredis_cache_SUITE.erl
@@ -20,20 +20,61 @@ end_per_suite(Config) ->
 
 all() ->
     [
-     t_get_cached
-    , t_get_expired
+     t_cached
+    , t_expired
+    , t_invalidate_single
+    , t_invalidate_multiple
+    , t_decorator_cached
+    , t_decorator_expired
     ].
 
 %% Tests
 
-t_get_cached(_Config) ->
+t_cached(_Config) ->
+    Key = <<"foo">>,
+    Value = <<"bar">>,
+    Opts = [{validity, ?CACHE_VALIDITY}],
+    ok = eredis_cache:set(eredis_cache_pool, Key, Value, Opts),
+    {ok, Value} = eredis_cache:get(eredis_cache_pool, Key).
+
+t_expired(_Config) ->
+    Key = <<"foo">>,
+    Value = <<"bar">>,
+    Opts = [{validity, ?CACHE_VALIDITY}],
+    ok = eredis_cache:set(eredis_cache_pool, Key, Value, Opts),
+    ok = timer:sleep(2000 * ?CACHE_VALIDITY),
+    {ok, undefined} = eredis_cache:get(eredis_cache_pool, Key),
+    ok.
+
+t_invalidate_single(_Config) ->
+    Key = <<"foo">>,
+    Value = <<"bar">>,
+    Opts = [{validity, 10 * ?CACHE_VALIDITY}],
+    ok = eredis_cache:set(eredis_cache_pool, Key, Value, Opts),
+    ok = eredis_cache:invalidate(eredis_cache_pool, Key),
+    {ok, undefined} = eredis_cache:get(eredis_cache_pool, Key),
+    ok.
+
+t_invalidate_multiple(_Config) ->
+    Key1 = <<"foo1">>,
+    Key2 = <<"foo2">>,
+    Value = <<"bar">>,
+    Opts = [{validity, 10 * ?CACHE_VALIDITY}],
+    ok = eredis_cache:set(eredis_cache_pool, Key1, Value, Opts),
+    ok = eredis_cache:set(eredis_cache_pool, Key2, Value, Opts),
+    ok = eredis_cache:invalidate(eredis_cache_pool, [Key1, Key2]),
+    {ok, undefined} = eredis_cache:get(eredis_cache_pool, Key1),
+    {ok, undefined} = eredis_cache:get(eredis_cache_pool, Key2),
+    ok.
+
+t_decorator_cached(_Config) ->
     Value = erlang:now(),
     {Value, Timestamp} = echo(Value),
     ok = timer:sleep(500 * ?CACHE_VALIDITY),
     {Value, Timestamp} = echo(Value),
     ok.
 
-t_get_expired(_Config) ->
+t_decorator_expired(_Config) ->
     Value = erlang:now(),
     {Value, Timestamp} = echo(Value),
     ok = timer:sleep(2000 * ?CACHE_VALIDITY),
