74ecc605fcb819da8254c1e2df2de7ddd239b943
Riccardo Massari <riccardo.massari@ubnt.com>
2015-05-26 19:13:27 +0200

    Enable custom timeout for poolboy gen_server calls

--- src/eredis_cache.erl
+++ src/eredis_cache.erl
@@ -22,6 +22,8 @@
          invalidate_pattern/3
         ]).
 
+-define(TIMEOUT, 15000).
+
 start_cache(CacheName, PoolArgs, WorkerArgs) ->
     eredis_pool_sup:create_pool(CacheName, PoolArgs, WorkerArgs).
 
@@ -29,7 +31,7 @@ stop_cache(CacheName) ->
     eredis_pool_sup:delete_pool(CacheName).
 
 get(PoolName, Key) when is_binary(Key) ->
-    case eredis_pool:q(PoolName, ["GET", Key]) of
+    case eredis_pool:q(PoolName, ["GET", Key], ?TIMEOUT) of
         {ok, undefined}=R -> R;
         {ok, Value} -> {ok, binary_to_term(Value)};
         {error, _}=E -> E
@@ -57,7 +59,7 @@ set(PoolName, Key, Value, Validity) when is_binary(Key),
     end.
 
 get_keys(PoolName, Pattern) when is_binary(Pattern) ->
-    eredis_pool:q(PoolName, ["KEYS", Pattern]).
+    eredis_pool:q(PoolName, ["KEYS", Pattern], ?TIMEOUT).
 
 invalidate(_PoolName, []) ->
     ok;
@@ -68,14 +70,14 @@ invalidate(PoolName, [Key | Rest]) ->
         {error, _}=E -> E
     end;
 invalidate(PoolName, Key) when is_binary(Key) ->
-    case eredis_pool:q(PoolName, ["DEL", Key]) of
+    case eredis_pool:q(PoolName, ["DEL", Key], ?TIMEOUT) of
         {ok, _} -> ok;
         {error, _}=E -> E
     end.
 
 invalidate_pattern(PoolName, Pattern) when is_binary(Pattern) ->
     Resp = eredis_pool:q(PoolName,
-                         ["SCAN", 0, "MATCH", Pattern]),
+                         ["SCAN", 0, "MATCH", Pattern], ?TIMEOUT),
     case Resp of
         {ok, [NextCursor, NextKeys]} ->
             invalidate_pattern(PoolName, Pattern, NextCursor, NextKeys);
@@ -97,7 +99,7 @@ invalidate_pattern(PoolName, Pattern, Cursor, Keys) ->
     case invalidate(PoolName, Keys) of
         ok ->
             Resp = eredis_pool:q(PoolName,
-                                 ["SCAN", Cursor, "MATCH", Pattern]),
+                                 ["SCAN", Cursor, "MATCH", Pattern], ?TIMEOUT),
             case Resp of
                 {ok, [NextCursor, NextKeys]} ->
                     invalidate_pattern(PoolName, Pattern, NextCursor, NextKeys);
